cdde90bacc65a0244ac3073c011040b222058e07
Lukasz Krefft <lukasz@snowy.pl>
2016-06-03 22:34:57 +0200

    Added max speed

--- GitFlowTest/src/pl/snowy/car/Car.java
+++ GitFlowTest/src/pl/snowy/car/Car.java
@@ -5,11 +5,14 @@ public class Car {
 	private String name;
 
 	private int hoursePowers;
+	
+	private int maxSpeed;
 
-	public Car(String name, int hoursePowers) {
+	public Car(String name, int hoursePowers, int maxSpeed) {
 		super();
 		this.name = name;
 		this.hoursePowers = hoursePowers;
+		this.maxSpeed = maxSpeed;
 	}
 
 	public String getName() {
@@ -28,9 +31,18 @@ public class Car {
 		this.hoursePowers = hoursePowers;
 	}
 
+	public int getMaxSpeed() {
+		return maxSpeed;
+	}
+
+	public void setMaxSpeed(int maxSpeed) {
+		this.maxSpeed = maxSpeed;
+	}
+
 	@Override
 	public String toString() {
-		return "Car [name=" + name + ", hoursePowers=" + hoursePowers + "]";
+		return "Car [name=" + name + ", hoursePowers=" + hoursePowers + ", maxSpeed=" + maxSpeed + "]";
 	}
+
 
 }
